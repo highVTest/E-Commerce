4726d743a9c2a4a0cb28b6f11be76f3d9d89c6bf
Ppajingae <wkmkyj991112@gmail.com>
2024-07-16 15:16:20 +0900

    ✨ feat: #4 Implementcoupon service createCoupon Method

--- src/main/kotlin/com/highv/ecommerce/common/dto/DefaultResponse.kt
+++ src/main/kotlin/com/highv/ecommerce/common/dto/DefaultResponse.kt
@@ -2,4 +2,8 @@ package com.highv.ecommerce.common.dto
 
 data class DefaultResponse(
     val msg: String,
-)
+){
+    companion object {
+        fun from(message: String) = DefaultResponse(msg = message)
+    }
+}
--- src/main/kotlin/com/highv/ecommerce/domain/coupon/service/CouponService.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/coupon/service/CouponService.kt
@@ -1,8 +1,32 @@
 package com.highv.ecommerce.domain.coupon.service
 
+import com.highv.ecommerce.common.dto.DefaultResponse
+import com.highv.ecommerce.domain.coupon.dto.CreateCouponRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Service
+@Transactional
 class CouponService {
+
+
+    fun createCoupon(couponRequest: CreateCouponRequest): DefaultResponse{
+
+        couponRepository.save(
+            Coupon(
+                productId = couponRequest.productId,
+                discountRate = couponRequest.discountRate,
+                discountPrice = couponRequest.discountPrice,
+                expiredAt = couponRequest.expiredAt,
+                quantity = couponRequest.quantity,
+                createdAt = LocalDateTime.now(),
+                isDeleted = false,
+                deletedAt = null
+            )
+        )
+
+        return DefaultResponse.from("쿠폰 생성이 완료 되었습니다")
+    }
 
 }
