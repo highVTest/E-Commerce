6d506dd3d875c016ca7a592c9a2e2780eda60bcf
Alice <hifumialice@gmail.com>
2024-07-16 16:03:07 +0900

    :sparkles: feat: Implement Item-Cart Repository

--- src/main/kotlin/com/highv/ecommerce/domain/item_cart/repository/ItemCartRepository.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/item_cart/repository/ItemCartRepository.kt
@@ -1,4 +1,8 @@
 package com.highv.ecommerce.domain.item_cart.repository
 
-class ItemCartRepository {
+import com.highv.ecommerce.domain.item_cart.entity.ItemCart
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ItemCartRepository: JpaRepository<ItemCart, Long> {
+
 }
