44b552a4c9f531e5e3eb4b31b534a078fea2ab1d
Ppajingae <wkmkyj991112@gmail.com>
2024-07-17 17:18:14 +0900

    ✨ feat: #7 Implement ProductsOrder Service structure

--- src/main/kotlin/com/highv/ecommerce/domain/products_order/controller/ProductsOrderController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/products_order/controller/ProductsOrderController.kt
@@ -1,5 +1,8 @@
 package com.highv.ecommerce.domain.products_order.controller
 
+import com.highv.ecommerce.common.dto.DefaultResponse
+import com.highv.ecommerce.domain.products_order.repository.ProductsOrderRepository
+import com.highv.ecommerce.domain.products_order.service.ProductsOrderService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,8 +13,12 @@ import kotlin.io.encoding.Base64
 
 @RestController
 @RequestMapping("/api/v1/")
-class ProductsOrderController {
+class ProductsOrderController(
+    private val productsOrderService: ProductsOrderService
+){
 
     @PostMapping("/payments/{cartId}")
-    fun requestPayment(@PathVariable("cartId") cartId: Long): ResponseEntity<DefaultResponse> = ResponseEntity.status(HttpStatus.OK).build()
+    fun requestPayment(@PathVariable("cartId") cartId: Long): ResponseEntity<DefaultResponse> {
+        return ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestPayment(cartId))
+    }
 }
---
+++ src/main/kotlin/com/highv/ecommerce/domain/products_order/service/ProductsOrderService.kt
@@ -0,0 +1,12 @@
+package com.highv.ecommerce.domain.products_order.service
+
+import com.highv.ecommerce.common.dto.DefaultResponse
+import org.springframework.stereotype.Service
+
+@Service
+class ProductsOrderService {
+
+    fun requestPayment(cartId: Long): DefaultResponse? {
+        TODO("Not yet implemented")
+    }
+}
