872a2c5cc0dea5d6a8c04a181049de68d05310c1
Alice <hifumialice@gmail.com>
2024-07-17 14:09:40 +0900

    :sparkles: feat: Update entity change to response

--- src/main/kotlin/com/highv/ecommerce/domain/item_cart/dto/response/ItemCartResponse.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/item_cart/dto/response/ItemCartResponse.kt
@@ -1,7 +1,17 @@
 package com.highv.ecommerce.domain.item_cart.dto.response
 
+import com.highv.ecommerce.domain.item_cart.entity.ItemCart
+
 data class ItemCartResponse(
     val productName: String,
     val productQuantity: Int,
     val totalPrice: Int
-)
+) {
+    companion object {
+        fun from(itemCart: ItemCart): ItemCartResponse = ItemCartResponse(
+            productName = itemCart.productName,
+            productQuantity = itemCart.quantity,
+            totalPrice = itemCart.price
+        )
+    }
+}
