312b25a64202590cfbfb3039f8ac266ea0f877ef
Alice <hifumialice@gmail.com>
2024-07-18 12:24:06 +0900

    :sparkles: feat: Update authorize

--- src/main/kotlin/com/highv/ecommerce/domain/item_cart/controller/ItemCartController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/item_cart/controller/ItemCartController.kt
@@ -4,8 +4,11 @@ import com.highv.ecommerce.domain.item_cart.dto.request.AddItemIntoCartRequest
 import com.highv.ecommerce.domain.item_cart.dto.request.UpdateItemIntoCartRequest
 import com.highv.ecommerce.domain.item_cart.dto.response.ItemCartResponse
 import com.highv.ecommerce.domain.item_cart.service.ItemCartService
+import com.highv.ecommerce.infra.security.UserPrincipal
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -23,35 +25,41 @@ class ItemCartController(
 ) {
 
     @PostMapping("/{productId}")
+    @PreAuthorize("hasRole('BUYER')")
     fun addItemIntoCart(
         @PathVariable(value = "productId") productId: Long,
-        @RequestParam(value = "buyerId") buyerId: Long, // 추후 인증 인가 완료시 삭제
+        @AuthenticationPrincipal user: UserPrincipal,
         @RequestBody request: AddItemIntoCartRequest
-    ): ResponseEntity<Unit> = ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(itemCartService.addItemIntoCart(productId, request, buyerId))
+    ): ResponseEntity<Unit> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(itemCartService.addItemIntoCart(productId, request, user.id))
+    }
 
+    @PreAuthorize("hasRole('BUYER')")
     @GetMapping()
     fun getMyCart(
-        @RequestParam(value = "buyerId") buyerId: Long, // 추후 인증 인가 완료시 삭제
+        @AuthenticationPrincipal user: UserPrincipal,
     ): ResponseEntity<List<ItemCartResponse>> = ResponseEntity
         .status(HttpStatus.OK)
-        .body(itemCartService.getMyCart(buyerId))
+        .body(itemCartService.getMyCart(user.id))
 
+    @PreAuthorize("hasRole('BUYER')")
     @PutMapping("/{productId}")
     fun updateIntoCart(
         @PathVariable(value = "productId") productId: Long,
-        @RequestParam(value = "buyerId") buyerId: Long, // 추후 인증 인가 완료시 삭제
+        @AuthenticationPrincipal user: UserPrincipal,
         @RequestBody request: UpdateItemIntoCartRequest
     ): ResponseEntity<Unit> = ResponseEntity
         .status(HttpStatus.OK)
-        .body(itemCartService.updateItemIntoCart(productId, request, buyerId))
+        .body(itemCartService.updateItemIntoCart(productId, request, user.id))
 
+    @PreAuthorize("hasRole('BUYER')")
     @DeleteMapping("/{productId}")
     fun deleteItemIntoCart(
         @PathVariable(value = "productId") productId: Long,
-        @RequestParam(value = "buyerId") buyerId: Long // 추후 인증 인가 완료시 삭제
+        @AuthenticationPrincipal user: UserPrincipal,
     ): ResponseEntity<Unit> = ResponseEntity
         .status(HttpStatus.NO_CONTENT)
-        .body(itemCartService.deleteItemIntoCart(productId, buyerId))
+        .body(itemCartService.deleteItemIntoCart(productId, user.id))
 }
