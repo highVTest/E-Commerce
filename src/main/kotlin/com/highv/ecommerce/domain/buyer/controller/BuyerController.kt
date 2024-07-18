cceafb7f23eab399b39e4bb1919c5ddb3cbb1a9c
Alice <hifumialice@gmail.com>
2024-07-18 11:08:39 +0900

    :sparkles: feat: Implement bad request error message return

--- src/main/kotlin/com/highv/ecommerce/domain/buyer/controller/BuyerController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/buyer/controller/BuyerController.kt
@@ -1,11 +1,13 @@
 package com.highv.ecommerce.domain.buyer.controller
 
+import com.highv.ecommerce.common.exception.LoginException
 import com.highv.ecommerce.domain.buyer.dto.BuyerResponse
 import com.highv.ecommerce.domain.buyer.dto.CreateBuyerRequest
 import com.highv.ecommerce.domain.buyer.service.BuyerService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +17,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/buyer")
 class BuyerController(private val buyerService: BuyerService) {
     @PostMapping("/user_signup")
-    fun signUp(@RequestBody @Valid request: CreateBuyerRequest): ResponseEntity<BuyerResponse> = ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(buyerService.signUp(request))
+    fun signUp(
+        @RequestBody @Valid request: CreateBuyerRequest,
+        bindingResult: BindingResult
+    ): ResponseEntity<BuyerResponse> {
+
+        if (bindingResult.hasErrors()) {
+            throw LoginException(bindingResult.fieldError?.defaultMessage.toString())
+        }
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(buyerService.signUp(request))
+    }
 }
--- src/main/kotlin/com/highv/ecommerce/domain/seller/dto/CreateSellerRequest.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/seller/dto/CreateSellerRequest.kt
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Size
 
 data class CreateSellerRequest(
     val nickname: String,
-    @field: NotBlank
-    @field: Size(min = 8, max = 30)
+    @field : NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+    @field : Size(min = 8, max = 30, message = "비밀번호는 최소 8자 이상 최대 30 이하입니다.")
     val password: String,
-    @field: NotBlank
-    @field:Email
+    @field: NotBlank(message = "이메일은 공백일 수 없습니다.")
+    @field:Email(message = "이메일 형식이 아닙니다.")
     val email: String,
     val profileImage: String,
     val phoneNumber: String,
