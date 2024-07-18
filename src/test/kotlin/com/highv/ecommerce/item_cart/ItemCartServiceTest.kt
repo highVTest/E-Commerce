71ca87d8ab0d87ea1fae050692d6d6f855c848cc
Alice <hifumialice@gmail.com>
2024-07-18 10:07:09 +0900

    :white_check_mark: test: Implement test Item-Cart service update func

--- src/test/kotlin/com/highv/ecommerce/item_cart/ItemCartServiceTest.kt
+++ src/test/kotlin/com/highv/ecommerce/item_cart/ItemCartServiceTest.kt
@@ -1,6 +1,7 @@
 package com.highv.ecommerce.item_cart
 
 import com.highv.ecommerce.domain.item_cart.dto.request.AddItemIntoCartRequest
+import com.highv.ecommerce.domain.item_cart.dto.request.UpdateItemIntoCartRequest
 import com.highv.ecommerce.domain.item_cart.entity.ItemCart
 import com.highv.ecommerce.domain.item_cart.repository.ItemCartRepository
 import com.highv.ecommerce.domain.item_cart.service.ItemCartService
@@ -31,6 +32,8 @@ class ItemCartServiceTest : BehaviorSpec() {
 
         this.Given("상품을 장바구니에 담을 때") {
 
+            val buyerId = 1L
+
             val product = Product(
                 name = "Test Product",
                 description = "Test Description",
@@ -47,7 +50,6 @@ class ItemCartServiceTest : BehaviorSpec() {
                 categoryId = 1L
             ).apply { id = 1L }
 
-            val buyerId = 1L
 
             When("상품 수량이 1개 이상이면") {
                 val request: AddItemIntoCartRequest = AddItemIntoCartRequest(quantity = 1)
@@ -93,6 +95,67 @@ class ItemCartServiceTest : BehaviorSpec() {
                         .let {
                             it.message shouldBe "Product not found"
                         }
+                }
+            }
+
+        }
+
+        this.Given("장바구니에 담긴 상품의 개수를 변경할 때") {
+
+            val buyerId = 1L
+
+            val product = Product(
+                name = "Test Product",
+                description = "Test Description",
+                price = 1000,
+                productImage = "image.jpg",
+                favorite = 0,
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now(),
+                quantity = 10,
+                isSoldOut = false,
+                deletedAt = LocalDateTime.now(),
+                isDeleted = false,
+                shopId = 1L,
+                categoryId = 1L
+            ).apply { id = 1L }
+
+            val itemCart: ItemCart = ItemCart(
+                productId = 1L,
+                productName = product.name,
+                price = 3000,
+                quantity = 5,
+                buyerId = buyerId
+            ).apply { id = 1L }
+
+
+
+            When("변경할 수량이 1개 이상이면") {
+                val request: UpdateItemIntoCartRequest = UpdateItemIntoCartRequest(quantity = 5)
+
+                every { productRepository.findByIdOrNull(any()) } returns product
+                every { itemCartRepository.findByProductIdAndBuyerIdAndIsDeletedFalse(any(), any()) } returns itemCart
+
+                Then("수량과 총액이 변경된다.") {
+                    every { itemCartRepository.save(any()) } returns itemCart.apply { 3000 * request.quantity }
+
+                    itemCartService.updateItemIntoCart(product.id!!, request, buyerId)
+                }
+
+            }
+
+            When("상품이 장바구니에 없으면") {
+                val request: UpdateItemIntoCartRequest = UpdateItemIntoCartRequest(quantity = 5)
+
+                every { productRepository.findByIdOrNull(any()) } returns product
+                every { itemCartRepository.findByProductIdAndBuyerIdAndIsDeletedFalse(any(), any()) } returns null
+
+                Then("상품이 없다는 예외가 발생한다.") {
+                    shouldThrow<RuntimeException> {
+                        itemCartService.updateItemIntoCart(product.id!!, request, buyerId)
+                    }.let {
+                        it.message shouldBe "Item not found"
+                    }
                 }
             }
 
