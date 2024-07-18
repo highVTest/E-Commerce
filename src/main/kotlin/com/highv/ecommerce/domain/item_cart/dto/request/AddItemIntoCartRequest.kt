91a3eabc20169426b726e505a7502f2756ffe189
Alice <hifumialice@gmail.com>
2024-07-18 14:11:46 +0900

    :fire: remove: Delete

--- src/main/kotlin/com/highv/ecommerce/domain/item_cart/dto/request/AddItemIntoCartRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.highv.ecommerce.domain.item_cart.dto.request
-
-data class AddItemIntoCartRequest(
-    val quantity: Int
-)
--- src/main/kotlin/com/highv/ecommerce/domain/item_cart/dto/request/UpdateItemIntoCartRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.highv.ecommerce.domain.item_cart.dto.request
-
-data class UpdateItemIntoCartRequest(
-    val quantity: Int
-)
