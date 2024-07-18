209053274702b06303bccb8ad5456e05cb578941
Alice <hifumialice@gmail.com>
2024-07-18 15:00:36 +0900

    :sparkles: feat: Implement Favorite entity

--- src/main/kotlin/com/highv/ecommerce/domain/favorite/entity/Favorite.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/favorite/entity/Favorite.kt
@@ -1,2 +1,25 @@
-package com.highv.ecommerce.domain.favorite.entity 
+package com.highv.ecommerce.domain.favorite.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "favorite")
+class Favorite(
+
+    @Column(name = "product_id", nullable = false)
+    val productId: Long,
+
+    @Column(name = "buyer_id", nullable = false)
+    val buyerId: Long,
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null
+}
