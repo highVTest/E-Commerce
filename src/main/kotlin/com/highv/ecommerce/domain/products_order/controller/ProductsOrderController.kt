f38d2a2e4d44d70cbd416745f399ff482e05c286
Ppajingae <wkmkyj991112@gmail.com>
2024-07-18 11:29:01 +0900

    ✨ feat: #7 Implement ProductsOrder controller updateOrderStatus

--- src/main/kotlin/com/highv/ecommerce/domain/products_order/controller/ProductsOrderController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/products_order/controller/ProductsOrderController.kt
@@ -1,14 +1,12 @@
 package com.highv.ecommerce.domain.products_order.controller
 
 import com.highv.ecommerce.common.dto.DefaultResponse
+import com.highv.ecommerce.domain.products_order.dto.OrderStatusRequest
 import com.highv.ecommerce.domain.products_order.repository.ProductsOrderRepository
 import com.highv.ecommerce.domain.products_order.service.ProductsOrderService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import kotlin.io.encoding.Base64
 
 @RestController
@@ -21,4 +19,14 @@ class ProductsOrderController(
     fun requestPayment(@PathVariable("cartId") cartId: Long): ResponseEntity<DefaultResponse> {
         return ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestPayment(cartId))
     }
+
+
+    @PatchMapping("/order_status/{orderId}")
+    fun updateOrderStatus(
+        @PathVariable("orderId") orderId: Long,
+        @RequestBody orderStatusRequest: OrderStatusRequest
+    ): ResponseEntity<DefaultResponse> {
+        return ResponseEntity.status(HttpStatus.OK).body(productsOrderService.updateOrderStatus(orderId, orderStatusRequest))
+    }
+
 }
