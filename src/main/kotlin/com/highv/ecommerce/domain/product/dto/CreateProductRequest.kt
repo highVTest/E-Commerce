c54b818ac447b25a63f34a90f4880f5d762f5c60
HR <dev.muun07@gmail.com>
2024-07-18 15:33:29 +0900

    ♻ refactor : Refactoring Product Domains (ERD)

--- src/main/kotlin/com/highv/ecommerce/domain/product/dto/CreateProductRequest.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/product/dto/CreateProductRequest.kt
@@ -2,13 +2,11 @@ package com.highv.ecommerce.domain.product.dto
 
 import java.time.LocalDateTime
 
-data class CreateProductRequest (
-    val name:String,
-    val description:String,
-    val price: Int,
-    val productImage:String,
+data class CreateProductRequest(
+    val name: String,
+    val description: String,
+    val productImage: String,
     val createdAt: LocalDateTime,
-    val quantity:Int,
-    val shopId:Long,
-    val categoryId:Long,
+    val shopId: Long,
+    val categoryId: Long
 )
--- src/main/kotlin/com/highv/ecommerce/domain/product/dto/UpdateProductRequest.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/product/dto/UpdateProductRequest.kt
@@ -2,13 +2,11 @@ package com.highv.ecommerce.domain.product.dto
 
 import java.time.LocalDateTime
 
-data class UpdateProductRequest (
+data class UpdateProductRequest(
     val name: String,
     val description: String,
-    val price: Int,
     val productImage: String,
     val updatedAt: LocalDateTime,
-    val quantity: Int,
     val isSoldOut: Boolean,
     val categoryId: Long,
 )
