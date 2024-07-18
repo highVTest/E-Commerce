e94bd654cd708964dface553c9deb93b1cd99161
Ppajingae <wkmkyj991112@gmail.com>
2024-07-18 11:37:41 +0900

    ✨ feat: #7 Implement ProductsOrder controller All

--- src/main/kotlin/com/highv/ecommerce/domain/products_order/controller/ProductsOrderController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/products_order/controller/ProductsOrderController.kt
@@ -1,6 +1,7 @@
 package com.highv.ecommerce.domain.products_order.controller
 
 import com.highv.ecommerce.common.dto.DefaultResponse
+import com.highv.ecommerce.domain.product.entity.Product
 import com.highv.ecommerce.domain.products_order.dto.DescriptionRequest
 import com.highv.ecommerce.domain.products_order.dto.OrderStatusRequest
 import com.highv.ecommerce.domain.products_order.repository.ProductsOrderRepository
@@ -17,29 +18,51 @@ class ProductsOrderController(
 ){
 
     @PostMapping("/payments/{cartId}")
-    fun requestPayment(@PathVariable("cartId") cartId: Long): ResponseEntity<DefaultResponse> {
-        return ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestPayment(cartId))
-    }
-
+    fun requestPayment(@PathVariable("cartId") cartId: Long): ResponseEntity<DefaultResponse>
+        = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestPayment(cartId))
 
     @PatchMapping("/order_status/{orderId}")
     fun updateOrderStatus(
         @PathVariable("orderId") orderId: Long,
         @RequestBody orderStatusRequest: OrderStatusRequest
-    ): ResponseEntity<DefaultResponse> {
-        return ResponseEntity.status(HttpStatus.OK).body(productsOrderService.updateOrderStatus(orderId, orderStatusRequest))
-    }
+    ): ResponseEntity<DefaultResponse>
+        = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.updateOrderStatus(orderId, orderStatusRequest))
 
     @PatchMapping("/refund/{orderId}")
     fun requestRefund(
         @PathVariable("orderId") orderId: Long,
         @RequestBody descriptionRequest: DescriptionRequest
-    ): ResponseEntity<DefaultResponse> {
-        return ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestRefund(orderId, descriptionRequest))
-    }
+    ): ResponseEntity<DefaultResponse>
+        = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestRefund(orderId, descriptionRequest))
 
+    @PatchMapping("/refund/reject/{orderId}")
+    fun requestRefundReject(
+        @PathVariable("orderId") orderId: Long,
+        @RequestBody descriptionRequest: DescriptionRequest
+    ): ResponseEntity<DefaultResponse>
+        = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestRefundReject(orderId, descriptionRequest))
 
+    @PatchMapping("/exchange/{orderId}")
+    fun requestExchange(
+        @PathVariable("orderId") orderId: Long,
+        @RequestBody descriptionRequest: DescriptionRequest
+    ): ResponseEntity<DefaultResponse>
+       = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestExchange(orderId, descriptionRequest))
 
+    @PatchMapping("/exchange/reject/{orderId}")
+    fun requestExchangeReject(
+        @PathVariable("orderId") orderId: Long,
+        @RequestBody descriptionRequest: DescriptionRequest
+    ): ResponseEntity<DefaultResponse>
+        = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestExchangeReject(orderId, descriptionRequest))
+
+    @GetMapping("/order_status/{orderId}")
+    fun getOrderDetails(@PathVariable("orderId") orderId: Long,): ResponseEntity<ProductsOrderResponse>
+        = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.getOrderDetails(orderId))
+
+    @PatchMapping("/order_cancelled/{orderId}")
+    fun requestOrderCanceled(@PathVariable("orderId") orderId: Long): ResponseEntity<DefaultResponse>
+        = ResponseEntity.status(HttpStatus.OK).body(productsOrderService.requestOrderCanceled(orderId))
 
 
 }
