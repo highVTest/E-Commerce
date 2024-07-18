6b89a1163842c3112dae6280ebd90876e3e8b75c
Alice <hifumialice@gmail.com>
2024-07-18 16:01:40 +0900

    :sparkles: feat: Implement favorite add and delete

--- src/main/kotlin/com/highv/ecommerce/domain/favorite/controller/FavoriteController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/favorite/controller/FavoriteController.kt
@@ -1,4 +1,28 @@
 package com.highv.ecommerce.domain.favorite.controller
 
-class FavoriteController {
+import com.highv.ecommerce.domain.favorite.service.FavoriteService
+import com.highv.ecommerce.infra.security.UserPrincipal
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/favorite")
+class FavoriteController(
+    private val favoriteService: FavoriteService
+) {
+
+    @PreAuthorize("hasRole('BUYER')")
+    @PostMapping("/{productId}")
+    fun favoriteManagement(
+        @PathVariable("productId") productId: Long,
+        @AuthenticationPrincipal user: UserPrincipal
+    ): ResponseEntity<String> = ResponseEntity
+        .status(HttpStatus.OK)
+        .body(favoriteService.management(productId, user))
 }
--- src/main/kotlin/com/highv/ecommerce/domain/favorite/service/FavoriteService.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/favorite/service/FavoriteService.kt
@@ -1,2 +1,43 @@
-package com.highv.ecommerce.domain.favorite.service 
+package com.highv.ecommerce.domain.favorite.service
 
+import com.highv.ecommerce.domain.buyer.repository.BuyerRepository
+import com.highv.ecommerce.domain.favorite.entity.Favorite
+import com.highv.ecommerce.domain.favorite.repository.FavoriteRepository
+import com.highv.ecommerce.domain.product.repository.ProductRepository
+import com.highv.ecommerce.infra.security.UserPrincipal
+import org.springframework.stereotype.Service
+
+@Service
+class FavoriteService(
+    private val favoriteRepository: FavoriteRepository,
+    private val productRepository: ProductRepository,
+    private val buyerRepository: BuyerRepository,
+) {
+
+    fun management(productId: Long, buyer: UserPrincipal): String {
+
+        if (!productRepository.existsById(productId)) {
+            throw RuntimeException("Product with ID $productId not found")
+        }
+
+        if (!buyerRepository.existsById(buyer.id)) {
+            throw RuntimeException("Buyer with ID ${buyer.id} not found")
+        }
+
+        val existsFavorite: Favorite? = favoriteRepository.findByProductIdAndBuyerId(productId, buyer.id)
+
+        if (existsFavorite != null) {
+            favoriteRepository.delete(existsFavorite)
+            return "찜 목록에서 삭제했습니다."
+        }
+
+        val favorite: Favorite = Favorite(
+            productId = productId,
+            buyerId = buyer.id
+        )
+
+        favoriteRepository.save(favorite)
+
+        return "찜 목록에 추가 했습니다."
+    }
+}
