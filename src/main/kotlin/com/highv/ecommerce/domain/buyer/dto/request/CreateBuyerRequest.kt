0ffb7c15802c062572f19ffb3b15f0113136192f
Alice <hifumialice@gmail.com>
2024-07-19 12:30:39 +0900

    :truck: rename: move dto package

--- src/main/kotlin/com/highv/ecommerce/domain/buyer/dto/request/CreateBuyerRequest.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/buyer/dto/request/CreateBuyerRequest.kt
@@ -1,4 +1,4 @@
-package com.highv.ecommerce.domain.buyer.dto
+package com.highv.ecommerce.domain.buyer.dto.request
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
