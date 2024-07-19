e6aa353df3aa49b037a0ac6799d598cf019426a9
HR <dev.muun07@gmail.com>
2024-07-19 23:22:12 +0900

    🔥 remove : Delete unused function

--- src/main/kotlin/com/highv/ecommerce/domain/backoffice/repository/ProductBackOfficeRepository.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/backoffice/repository/ProductBackOfficeRepository.kt
@@ -3,6 +3,4 @@ package com.highv.ecommerce.domain.backoffice.repository
 import com.highv.ecommerce.domain.backoffice.entity.ProductBackOffice
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProductBackOfficeRepository : JpaRepository<ProductBackOffice, Long> {
-    fun findAllByShopId(shopId: Long): List<ProductBackOffice>
-}
+interface ProductBackOfficeRepository : JpaRepository<ProductBackOffice, Long>
