eebacd16d3d12ebe45f7ba30dd73d321547c1c3e
Alice <hifumialice@gmail.com>
2024-07-19 12:31:18 +0900

    :sparkles: feat: Implement user change password

--- src/main/kotlin/com/highv/ecommerce/domain/buyer/controller/BuyerController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/buyer/controller/BuyerController.kt
@@ -1,13 +1,18 @@
 package com.highv.ecommerce.domain.buyer.controller
 
 import com.highv.ecommerce.common.exception.LoginException
-import com.highv.ecommerce.domain.buyer.dto.BuyerResponse
-import com.highv.ecommerce.domain.buyer.dto.CreateBuyerRequest
+import com.highv.ecommerce.domain.buyer.dto.request.CreateBuyerRequest
+import com.highv.ecommerce.domain.buyer.dto.request.UpdateBuyerPasswordRequest
+import com.highv.ecommerce.domain.buyer.dto.response.BuyerResponse
 import com.highv.ecommerce.domain.buyer.service.BuyerService
+import com.highv.ecommerce.infra.security.UserPrincipal
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -29,5 +34,22 @@ class BuyerController(private val buyerService: BuyerService) {
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(buyerService.signUp(request))
+    }
+
+    @PreAuthorize("hasRole('BUYER')")
+    @PatchMapping("/profile/password")
+    fun changePassword(
+        @Valid @RequestBody request: UpdateBuyerPasswordRequest,
+        bindingResult: BindingResult,
+        @AuthenticationPrincipal user: UserPrincipal,
+    ): ResponseEntity<Unit> {
+
+        if (bindingResult.hasErrors()) {
+            throw RuntimeException(bindingResult.fieldError?.defaultMessage.toString())
+        }
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(buyerService.changePassword(request, user.id))
     }
 }
--- src/main/kotlin/com/highv/ecommerce/domain/buyer/entity/Buyer.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/buyer/entity/Buyer.kt
@@ -1,27 +1,40 @@
 package com.highv.ecommerce.domain.buyer.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "buyer")
 class Buyer(
-   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-   val id: Long? = null,
-   @Column(name = "nickname")
-   val nickname: String,
-   @Column(name = "password")
-   val password: String,
-   @Column(name = "email")
-   val email: String,
-   @Column(name = "profile_image")
-   val profileImage: String,
-   @Column(name = "phone_number")
-   val phoneNumber: String,
-   @Column(name = "address")
-   val address: String,
 
-   @Column(name = "provider_name")
-   val providerName: String?,
-   @Column(name = "provider_id")
-   val providerId: Long?
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "nickname")
+    var nickname: String,
+
+    @Column(name = "password")
+    var password: String,
+
+    @Column(name = "email")
+    var email: String,
+
+    @Column(name = "profile_image")
+    var profileImage: String,
+
+    @Column(name = "phone_number")
+    var phoneNumber: String,
+
+    @Column(name = "address")
+    var address: String,
+
+    @Column(name = "provider_name")
+    val providerName: String?,
+
+    @Column(name = "provider_id")
+    val providerId: Long?
 )
