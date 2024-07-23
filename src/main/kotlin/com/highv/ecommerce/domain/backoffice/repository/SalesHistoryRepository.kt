32d9dc525f7cb29f6b71a9352eb1b9f49eab3bed
HR <dev.muun07@gmail.com>
2024-07-23 16:25:30 +0900

    ✨ feat: Implement SalesHistory

--- src/main/kotlin/com/highv/ecommerce/domain/backoffice/repository/SalesHistoryRepository.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/backoffice/repository/SalesHistoryRepository.kt
@@ -3,5 +3,6 @@ package com.highv.ecommerce.domain.backoffice.repository
 import com.highv.ecommerce.domain.backoffice.entity.SalesHistory
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface SalesHistoryRepository: JpaRepository<SalesHistory, Long> {
+interface SalesHistoryRepository : JpaRepository<SalesHistory, Long> {
+    fun findByOrderId(orderId: Long): SalesHistory
 }
