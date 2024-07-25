4a9750bae6a52bb7aa2b300fab4e164f3557228a
1hyung <ryu1hyung@naver.com>
2024-07-25 16:34:01 +0900

    :sparkles: Implement BlackList Entity

---
+++ src/main/kotlin/com/highv/ecommerce/domain/backoffice/admin/entity/BlackList.kt
@@ -0,0 +1,22 @@
+package com.highv.ecommerce.domain.backoffice.admin.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "black_list")
+class BlackList (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "nickname")
+    val nickname: String,
+
+    @Column(name = "email")
+    val email: String
+)
