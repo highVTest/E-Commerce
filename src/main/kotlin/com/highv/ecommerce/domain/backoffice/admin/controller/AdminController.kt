feb28f030b1067fbb593b0d9d98b531caad9f710
1hyung <ryu1hyung@naver.com>
2024-07-25 17:53:08 +0900

    :recycle: refactor: refactor code

--- src/main/kotlin/com/highv/ecommerce/domain/backoffice/admin/controller/AdminController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/backoffice/admin/controller/AdminController.kt
@@ -25,7 +25,9 @@ class AdminController(
     @PreAuthorize("hasRole('ADMIN')")
     fun sanctionSeller(@PathVariable sellerId: Long): ResponseEntity<DefaultResponse> {
         adminService.sanctionSeller(sellerId)
-        return ResponseEntity.status(HttpStatus.OK).body(DefaultResponse("판매자 제재 완료"))
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(DefaultResponse("판매자 제재 완료"))
     }
 
     // 상품 제재
@@ -33,7 +35,9 @@ class AdminController(
     @PreAuthorize("hasRole('ADMIN')")
     fun sanctionProduct(@PathVariable productId: Long): ResponseEntity<DefaultResponse> {
         adminService.sanctionProduct(productId)
-        return ResponseEntity.status(HttpStatus.OK).body(DefaultResponse("상품 제재 완료"))
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(DefaultResponse("상품 제재 완료"))
     }
 
     // // 구매자 제재 (미구현)
@@ -41,7 +45,9 @@ class AdminController(
     // @PreAuthorize("hasRole('ADMIN')")
     // fun sanctionBuyer(@PathVariable buyerId: Long): ResponseEntity<DefaultResponse> {
     //     adminService.sanctionBuyer(buyerId)
-    //     return ResponseEntity.status(HttpStatus.OK).body(DefaultResponse("구매자 제재 완료"))
+    //     return ResponseEntity
+    //     .status(HttpStatus.OK)
+    //     .body(DefaultResponse("구매자 제재 완료"))
     // }
 
     // 블랙리스트 생성
@@ -49,7 +55,9 @@ class AdminController(
     @PreAuthorize("hasRole('ADMIN')")
     fun createBlackList(@RequestBody request: CreateBlackListRequest): ResponseEntity<DefaultResponse> {
         adminService.createBlackList(request)
-        return ResponseEntity.status(HttpStatus.CREATED).body(DefaultResponse("블랙리스트 생성 완료"))
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(DefaultResponse("블랙리스트 생성 완료"))
     }
 
     // 블랙리스트 조회
@@ -57,7 +65,9 @@ class AdminController(
     @PreAuthorize("hasRole('ADMIN')")
     fun getBlackLists(): ResponseEntity<List<BlackListResponse>> {
         val blackLists = adminService.getBlackLists()
-        return ResponseEntity.status(HttpStatus.OK).body(blackLists)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(blackLists)
     }
 
     // 블랙리스트 단건 조회
@@ -65,7 +75,9 @@ class AdminController(
     @PreAuthorize("hasRole('ADMIN')")
     fun getBlackList(@PathVariable blackListId: Long): ResponseEntity<BlackListResponse> {
         val blackList = adminService.getBlackList(blackListId)
-        return ResponseEntity.status(HttpStatus.OK).body(blackList)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(blackList)
     }
 
     // 블랙리스트 삭제
@@ -73,6 +85,8 @@ class AdminController(
     @PreAuthorize("hasRole('ADMIN')")
     fun deleteBlackList(@PathVariable blackListId: Long): ResponseEntity<DefaultResponse> {
         adminService.deleteBlackList(blackListId)
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(DefaultResponse("블랙리스트 삭제 완료"))
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .body(DefaultResponse("블랙리스트 삭제 완료"))
     }
 }
