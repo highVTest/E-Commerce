99163cf8acf48e502adfe15e2620068b68784dae
1hyung <ryu1hyung@naver.com>
2024-07-25 16:34:14 +0900

    :sparkles: Implement BlackList Repository

---
+++ src/main/kotlin/com/highv/ecommerce/domain/backoffice/admin/repository/BlackListRepository.kt
@@ -0,0 +1,6 @@
+package com.highv.ecommerce.domain.backoffice.admin.repository
+
+import com.highv.ecommerce.domain.backoffice.admin.entity.BlackList
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface BlackListRepository : JpaRepository<BlackList, Long>
