fed0fecb7358f212ce0f89133c4f9f894129f84d
Alice <hifumialice@gmail.com>
2024-07-30 17:06:39 +0900

    :recycle: refactor: Refactor review query decrease

---
+++ src/main/kotlin/com/highv/ecommerce/domain/review/repository/ReviewQueryDslRepositoryImpl.kt
@@ -0,0 +1,49 @@
+package com.highv.ecommerce.domain.review.repository
+
+import com.highv.ecommerce.domain.review.entity.QReview.review
+import com.highv.ecommerce.domain.review.entity.Review
+import com.querydsl.jpa.impl.JPAQueryFactory
+
+class ReviewQueryDslRepositoryImpl(
+    private val queryFactory: JPAQueryFactory
+) : ReviewQueryDslRepository {
+    override fun findByIdOrNull(id: Long): Review? {
+        val query = queryFactory
+            .select(review)
+            .from(review)
+            .innerJoin(review.product()).fetchJoin()
+            .innerJoin(review.product().productBackOffice()).fetchJoin()
+            .innerJoin(review.product().shop()).fetchJoin()
+            .where(review.id.eq(id))
+            .fetchOne()
+
+        return query
+    }
+
+    override fun findAllByProductId(productId: Long): List<Review> {
+        val query = queryFactory
+            .select(review)
+            .from(review)
+            .innerJoin(review.product()).fetchJoin()
+            .innerJoin(review.product().productBackOffice()).fetchJoin()
+            .innerJoin(review.product().shop()).fetchJoin()
+            .where(review.product().id.eq(productId))
+            .fetch()
+
+        return query
+    }
+
+    override fun findAllByBuyerId(buyerId: Long): List<Review> {
+        val query = queryFactory
+            .select(review)
+            .from(review)
+            .innerJoin(review.product()).fetchJoin()
+            .innerJoin(review.product().productBackOffice()).fetchJoin()
+            .innerJoin(review.product().shop()).fetchJoin()
+            .where(review.buyerId.eq(buyerId))
+            .fetch()
+
+        return query
+    }
+}
+
--- src/main/kotlin/com/highv/ecommerce/domain/review/repository/ReviewRepository.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/review/repository/ReviewRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ReviewRepository: JpaRepository<Review, Long> {
-    fun findAllByProductId(productId: Long): List<Review>
-    fun findAllByBuyerId(buyerId: Long): List<Review>
+interface ReviewRepository : JpaRepository<Review, Long>, ReviewQueryDslRepository {
+    // fun findAllByProductId(productId: Long): List<Review>
+    // fun findAllByBuyerId(buyerId: Long): List<Review>
 }
