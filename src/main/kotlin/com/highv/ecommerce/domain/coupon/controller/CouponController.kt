67586b91c92c1b85c8c27003e38dc386c6ed265c
Ppajingae <wkmkyj991112@gmail.com>
2024-07-30 12:07:22 +0900

    :bug: fix conflict solve

--- src/main/kotlin/com/highv/ecommerce/domain/coupon/controller/CouponController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/coupon/controller/CouponController.kt
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 
