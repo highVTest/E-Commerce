d4570b6c4c4d6aa76c4eeae133ad94a3964c0c94
HR <dev.muun07@gmail.com>
2024-07-30 19:54:16 +0900

    ✨ feat: Implement "GET" SellerInfo

--- src/main/kotlin/com/highv/ecommerce/domain/seller/controller/SellerController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/seller/controller/SellerController.kt
@@ -4,9 +4,14 @@ import com.highv.ecommerce.common.exception.LoginException
 import com.highv.ecommerce.domain.seller.dto.CreateSellerRequest
 import com.highv.ecommerce.domain.seller.dto.SellerResponse
 import com.highv.ecommerce.domain.seller.service.SellerService
+import com.highv.ecommerce.domain.seller.shop.dto.CreateShopRequest
+import com.highv.ecommerce.domain.seller.shop.dto.ShopResponse
+import com.highv.ecommerce.infra.security.UserPrincipal
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -32,4 +37,14 @@ class SellerController(private val sellerService: SellerService) {
             .status(HttpStatus.CREATED)
             .body(sellerService.signUp(request, file))
     }
+
+    @PostMapping
+    @PreAuthorize("hasRole('SELLER')")
+    fun createShop(
+        @AuthenticationPrincipal seller: UserPrincipal,
+        @RequestPart createShopRequest: CreateShopRequest,
+        @RequestPart(value = "file", required = false) file: MultipartFile?
+    ): ResponseEntity<ShopResponse> = ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(sellerService.createShop(seller.id, createShopRequest, file))
 }
