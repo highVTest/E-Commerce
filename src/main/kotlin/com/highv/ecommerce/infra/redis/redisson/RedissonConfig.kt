6dd2599aa76e025b57ad316b17b272b6f1a30389
Ppajingae <wkmkyj991112@gmail.com>
2024-07-31 12:06:43 +0900

    :bug: fix fixed Redisson_URL

--- src/main/kotlin/com/highv/ecommerce/infra/redis/redisson/RedissonConfig.kt
+++ src/main/kotlin/com/highv/ecommerce/infra/redis/redisson/RedissonConfig.kt
@@ -3,20 +3,28 @@ package com.highv.ecommerce.infra.redis.redisson
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
 import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 const val REDISSON_HOST_PREFIX = "redis://"
 
 @Configuration
-class RedissonConfig {
+class RedissonConfig(
+
+    @Value("\${spring.data.redis.host}")
+    private val host: String,
+
+    @Value("\${spring.data.redis.port}")
+    private val port: Int,
+){
 
     @Bean
     fun redissonClient(): RedissonClient {
 
         val config = Config()
 
-        config.useSingleServer().setAddress("${REDISSON_HOST_PREFIX}localhost:6379")
+        config.useSingleServer().setAddress("${REDISSON_HOST_PREFIX}${host}:${port}")
 
         return Redisson.create(config)
     }
