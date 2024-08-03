f23c74c9cc16fab19ae5abea60b53a353702cc60
1hyung <ryu1hyung@naver.com>
2024-08-03 19:16:33 +0900

    :bug: Fix: fix data connect

--- src/main/kotlin/com/highv/ecommerce/domain/auth/controller/UserController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/auth/controller/UserController.kt
@@ -11,12 +11,14 @@ import com.highv.ecommerce.domain.auth.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = ["http://localhost:5173", "*"])
 @RestController
 @RequestMapping("/api/v1")
 class UserController(
--- src/main/kotlin/com/highv/ecommerce/domain/product/controller/ProductController.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/product/controller/ProductController.kt
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
+@CrossOrigin(origins = ["http://localhost:5173", "*"])
 @RestController
 @RequestMapping("/api/v1/products")
 class ProductController(
