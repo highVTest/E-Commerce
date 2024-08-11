806820f0d9ef7fb9788c96e744f15d428eb175e3
Alice <hifumialice@gmail.com>
2024-08-11 14:28:05 +0900

    :bug: fix: Fix favorite delete logic revise

--- src/main/kotlin/com/highv/ecommerce/domain/favorite/repository/FavoriteRepository.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/favorite/repository/FavoriteRepository.kt
@@ -2,9 +2,17 @@ package com.highv.ecommerce.domain.favorite.repository
 
 import com.highv.ecommerce.domain.favorite.entity.Favorite
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface FavoriteRepository : JpaRepository<Favorite, Long> {
-    fun findByProductIdAndBuyerId(productId: Long, id: Long): Favorite?
+    fun findAllByProductIdAndBuyerId(productId: Long, id: Long): List<Favorite>
+
+    fun existsByProductIdAndBuyerId(productId: Long, buyerId: Long): Boolean
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Favorite f where f.productId = :productId and f.buyerId = :buyerId")
+    fun deleteFavorite(productId: Long, buyerId: Long)
 
     fun findAllByBuyerId(buyerId: Long): List<Favorite>
 
