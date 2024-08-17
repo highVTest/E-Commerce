7bde3e4ab65bfc999d76bef8ecf0555d2b19212f
HR <dev.muun07@gmail.com>
2024-08-17 15:08:22 +0900

    ♻refactor: Refactor review Code

--- src/main/kotlin/com/highv/ecommerce/domain/review/repository/ReviewRepository.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/review/repository/ReviewRepository.kt
@@ -1,11 +1,54 @@
 package com.highv.ecommerce.domain.review.repository
 
+import com.highv.ecommerce.domain.review.entity.QReview.review
 import com.highv.ecommerce.domain.review.entity.Review
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ReviewRepository : JpaRepository<Review, Long>, ReviewQueryDslRepository {
-    // fun findAllByProductId(productId: Long): List<Review>
-    // fun findAllByBuyerId(buyerId: Long): List<Review>
+interface ReviewRepository : JpaRepository<Review, Long>, ReviewQueryDslRepository
+
+interface ReviewQueryDslRepository {
+    fun findByIdOrNull(id: Long): Review?
+    fun findAllByProductId(productId: Long): List<Review>
+    fun findAllByBuyerId(buyerId: Long): List<Review>
+    fun findAllByShopId(shopId: Long): List<Review>
+}
+
+class ReviewQueryDslRepositoryImpl(
+    private val queryFactory: JPAQueryFactory
+) : ReviewQueryDslRepository {
+    override fun findByIdOrNull(id: Long): Review? {
+        return queryFactory
+            .selectFrom(review)
+            .innerJoin(review.product()).fetchJoin()
+            .where(review.id.eq(id))
+            .fetchOne()
+    }
+
+    override fun findAllByProductId(productId: Long): List<Review> {
+        return queryFactory
+            .selectFrom(review)
+            .innerJoin(review.product()).fetchJoin()
+            .innerJoin(review.buyer()).fetchJoin()
+            .where(review.product().id.eq(productId))
+            .fetch()
+    }
+
+    override fun findAllByBuyerId(buyerId: Long): List<Review> {
+        return queryFactory
+            .selectFrom(review)
+            .innerJoin(review.product()).fetchJoin()
+            .where(review.buyer().id.eq(buyerId))
+            .fetch()
+    }
+
+    override fun findAllByShopId(shopId: Long): List<Review> {
+        return queryFactory
+            .selectFrom(review)
+            .innerJoin(review.product()).fetchJoin()
+            .where(review.product().shop().id.eq(shopId))
+            .fetch()
+    }
 }
--- src/main/kotlin/com/highv/ecommerce/domain/review/service/ReviewService.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/review/service/ReviewService.kt
@@ -1,8 +1,11 @@
 package com.highv.ecommerce.domain.review.service
 
 import com.highv.ecommerce.common.dto.DefaultResponse
-import com.highv.ecommerce.common.exception.*
-import com.highv.ecommerce.domain.buyer.entity.Buyer
+import com.highv.ecommerce.common.exception.BuyerNotFoundException
+import com.highv.ecommerce.common.exception.CustomRuntimeException
+import com.highv.ecommerce.common.exception.ProductNotFoundException
+import com.highv.ecommerce.common.exception.ReviewNotFoundException
+import com.highv.ecommerce.common.exception.ShopNotFoundException
 import com.highv.ecommerce.domain.buyer.repository.BuyerRepository
 import com.highv.ecommerce.domain.product.repository.ProductRepository
 import com.highv.ecommerce.domain.review.dto.ReviewRequest
@@ -11,6 +14,7 @@ import com.highv.ecommerce.domain.review.entity.Review
 import com.highv.ecommerce.domain.review.repository.ReviewRepository
 import com.highv.ecommerce.domain.seller.shop.repository.ShopRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import kotlin.math.round
 
@@ -22,11 +26,10 @@ class ReviewService(
     private val buyerRepository: BuyerRepository,
 ) {
     fun addReview(productId: Long, reviewRequest: ReviewRequest, buyerId: Long): DefaultResponse {
+        val buyer = buyerRepository.findByIdOrNull(buyerId)
+            ?: throw BuyerNotFoundException(404, "Buyer id $buyerId not found")
         val product = productRepository.findByIdOrNull(productId)
             ?: throw ProductNotFoundException(404, "Product id $productId not found")
-        val buyer = buyerRepository.findByIdOrNull(buyerId)!!
-
-
         val review = Review(
             buyer = buyer,
             product = product,
@@ -36,7 +39,7 @@ class ReviewService(
 
         reviewRepository.saveAndFlush(review)
         updateShopAverageRate(productId)
-        return DefaultResponse("리뷰가 추가되었습니다.")
+        return DefaultResponse("리뷰가 등록되었습니다.")
     }
 
     fun updateReview(
@@ -45,44 +48,25 @@ class ReviewService(
         reviewRequest: ReviewRequest,
         buyerId: Long
     ): DefaultResponse {
-        val review = reviewRepository.findByIdOrNull(reviewId) ?: throw ReviewNotFoundException(
-            404,
-            "Review id $reviewId not found"
-        )
-
-        if (review.buyer.id != buyerId) {
-            throw CustomRuntimeException(400, "자기 리뷰가 아닙니다.")
-        }
-
+        val review = validateBuyer(reviewId, buyerId)
         review.apply {
             rate = reviewRequest.rate
             content = reviewRequest.content
         }
         reviewRepository.saveAndFlush(review)
-        updateShopAverageRate(productId)
         return DefaultResponse("리뷰가 수정되었습니다.")
     }
 
     fun deleteReview(productId: Long, reviewId: Long, buyerId: Long): DefaultResponse {
-        val review = reviewRepository.findByIdOrNull(reviewId) ?: throw ReviewNotFoundException(
-            404,
-            "Review id $reviewId not found"
-        )
-
-        if (review.buyer.id != buyerId) {
-            throw CustomRuntimeException(400, "자기 리뷰가 아닙니다.")
-        }
-        
+        val review = validateBuyer(reviewId, buyerId)
         reviewRepository.delete(review)
         updateShopAverageRate(productId)
-        return DefaultResponse("Review deleted successfully")
+        return DefaultResponse("리뷰가 삭제되었습니다.")
     }
 
     fun getProductReviews(productId: Long): List<ReviewResponse> {
-
         val reviews = reviewRepository.findAllByProductId(productId)
         return reviews.map { ReviewResponse.from(it) }
-
     }
 
     fun getBuyerReviews(buyerId: Long): List<ReviewResponse> {
@@ -90,23 +74,27 @@ class ReviewService(
         return reviews.map { ReviewResponse.from(it) }
     }
 
-    fun updateShopAverageRate(productId: Long) {
-        val reviews = reviewRepository.findAllByProductId(productId)
-        val shopId = productRepository.findByIdOrNull(productId)?.shop?.id ?: throw ProductNotFoundException(
-            404,
-            "Product id $productId not found"
-        )
-        val shop = shopRepository.findByIdOrNull(shopId) ?: throw ShopNotFoundException(
-            404,
-            "Shop not found for this product and shop"
-        )
+    fun validateBuyer(reviewId: Long, buyerId: Long): Review {
+        val review = reviewRepository.findByIdOrNull(reviewId)
+            ?: throw ReviewNotFoundException(404, "Review id $reviewId not found")
+        if (review.buyer.id != buyerId) throw CustomRuntimeException(400, "본인이 작성한 리뷰가 아닙니다.")
+        return review
+    }
 
-        val avgRate = if (reviews.isNotEmpty()) {
-            reviews.map { it.rate }.average().toFloat()
-        } else {
-            0f
+    @Scheduled(cron = "0 0 * * * *")
+    fun updateAllShopsAverageRate() {
+        val shops = shopRepository.findAll()
+        for (shop in shops) {
+            updateShopAverageRate(shop.id!!)
         }
+    }
 
+    fun updateShopAverageRate(shopId: Long) {
+        val shop = shopRepository.findByIdOrNull(shopId)
+            ?: throw ShopNotFoundException(404, "Shop not found for this product and shop")
+        val reviews = reviewRepository.findAllByShopId(shopId)
+
+        val avgRate = if (reviews.isNotEmpty()) reviews.map { it.rate }.average().toFloat() else 0f
         shop.rate = round(avgRate * 100) / 100
         shopRepository.save(shop)
     }
