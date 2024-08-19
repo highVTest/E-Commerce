b37d8c19bb80295b7a29a281f9b2c27a3fc28865
Alice <hifumialice@gmail.com>
2024-08-19 23:35:02 +0900

    :bug: fix: Fix buyer response and controller path-variable

--- src/main/kotlin/com/highv/ecommerce/domain/review/dto/BuyerReviewResponse.kt
+++ src/main/kotlin/com/highv/ecommerce/domain/review/dto/BuyerReviewResponse.kt
@@ -1,20 +1,20 @@
 package com.highv.ecommerce.domain.review.dto
 
-import com.highv.ecommerce.domain.buyer.entity.Buyer
 import com.highv.ecommerce.domain.product.dto.ReviewProductDto
 import com.highv.ecommerce.domain.review.entity.Review
 
 data class BuyerReviewResponse(
     val id: Long,
+    val productId: Long,
     val rate: Float,
     val content: String,
     val productName: String,
     val productImage: String,
-)
-{
+) {
     companion object {
-        fun from(review: Review, reviewProductDto: ReviewProductDto)=BuyerReviewResponse(
+        fun from(review: Review, reviewProductDto: ReviewProductDto) = BuyerReviewResponse(
             id = review.id!!,
+            productId = reviewProductDto.productId,
             rate = review.rate,
             content = review.content,
             productName = reviewProductDto.productName,
